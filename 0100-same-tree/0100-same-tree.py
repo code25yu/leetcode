87a7f0c849b0d385f4a884b4a050dbe2ab6901f3
code25yu <code25yu@gmail.com>
2025-08-03 08:57:02 +0900

    Time: 0 ms (100%), Space: 12.6 MB (54.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,12 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# # recursive solution
+# class Solution(object):
+#     def isSameTree(self, p, q):
+#         if not p and not q:
+#             return True 
+#         if not p or not q or p.val != q.val:
+#             return False
+#         return (self.isSameTree(p.left, q.left) and
+#             self.isSameTree(p.right, q.right))
+
+# BFS(queue)
+from collections import deque
+
 class Solution(object):
     def isSameTree(self, p, q):
-        if not p and not q:
-            return True 
-        if not p or not q or p.val != q.val:
-            return False
-        return (self.isSameTree(p.left, q.left) and
-            self.isSameTree(p.right, q.right))
-        
+        queue = deque()
+        queue.append((p, q))
+
+        while queue:
+            node1, node2 = queue.popleft()
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+        return True
