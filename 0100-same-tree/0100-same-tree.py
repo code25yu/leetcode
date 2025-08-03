e5673a6ca44424a28d06a236e6143dc5f2671a9d
code25yu <code25yu@gmail.com>
2025-08-03 09:44:32 +0900

    Time: 0 ms (100%), Space: 12.5 MB (54.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -10,27 +10,44 @@
 #     def isSameTree(self, p, q):
 #         if not p and not q:
 #             return True 
-#         if not p or not q or p.val != q.val:
+#         if not p or not qp.val != q.val:
 #             return False
 #         return (self.isSameTree(p.left, q.left) and
 #             self.isSameTree(p.right, q.right))
 
-# BFS(queue)
-from collections import deque
+# # BFS(queue)
+# from collections import deque
 
+# class Solution(object):
+#     def isSameTree(self, p, q):
+#         queue = deque()
+#         queue.append((p, q))
+
+#         while queue:
+#             node1, node2 = queue.popleft()
+#             if not node1 and not node2:
+#                 continue
+#             if not node1 or not node2:
+#                 return False
+#             if node1.val != node2.val:
+#                 return False
+#             queue.append((node1.left, node2.left))
+#             queue.append((node1.right, node2.right))
+#         return True
+
+# DFS(stack)
 class Solution(object):
     def isSameTree(self, p, q):
-        queue = deque()
-        queue.append((p, q))
-
-        while queue:
-            node1, node2 = queue.popleft()
+        stack = [(p, q)]
+        while stack:
+            node1, node2 = stack.pop()
             if not node1 and not node2:
                 continue
             if not node1 or not node2:
                 return False
             if node1.val != node2.val:
                 return False
-            queue.append((node1.left, node2.left))
-            queue.append((node1.right, node2.right))
+            stack.append((node1.left, node2.left))
+            stack.append((node1.right, node2.right))
         return True
+
