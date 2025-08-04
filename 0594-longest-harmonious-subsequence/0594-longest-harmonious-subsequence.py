224337745a294239ea0ee2b77a8763bf5edaa999
code25yu <code25yu@gmail.com>
2025-08-04 13:26:52 +0900

    Time: 81 ms (17.06%), Space: 13.5 MB (79.09%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,9 +1,25 @@
+# hash table 
+# class Solution(object):
+#     def findLHS(self, nums):
+#         max_len = 0
+#         freq = Counter(nums)
+#         for key in freq:
+#             if key + 1 in freq:
+#                 now_len = freq[key] + freq[key + 1]
+#                 max_len = max(max_len, now_len)
+#         return max_len
+
+# sort + two pointers / sliding window
 class Solution(object):
     def findLHS(self, nums):
         max_len = 0
-        freq = Counter(nums)
-        for key in freq:
-            if key + 1 in freq:
-                now_len = freq[key] + freq[key + 1]
-                max_len = max(max_len, now_len)
+        start = 0
+        nums.sort()
+        for end in range(len(nums)):
+            while nums[end] - nums[start] > 1:
+                start += 1
+            if nums[end] - nums[start] == 1:
+                max_len = max(max_len, end - start + 1)
         return max_len
+                
+        
