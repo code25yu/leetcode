09fefde7c6d94dc5fb08b04e4fdbccda1f89c8e7
code25yu <code25yu@gmail.com>
2025-08-06 12:29:56 +0900

    Time: 0 ms (100%), Space: 12.7 MB (23.2%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -9,15 +9,32 @@
 # :type root: Optional[TreeNode]
 # :rtype: bool
 # """
+
+# DFS
+# class Solution(object):
+#     def isSymmetric(self, root):
+#         def isMirror(t1, t2):
+#             if not t1 and not t2:
+#                 return True
+#             if not t1 or not t2:
+#                 return False
+#             return (t1.val == t2.val and 
+#                     isMirror(t1.left, t2.right) and
+#                     isMirror(t1.right, t2.left))
+#         return isMirror(root.left, root.right)
+        
+# BFS
 class Solution(object):
     def isSymmetric(self, root):
-        def isMirror(t1, t2):
+        queue = deque([(root.left, root.right)])
+        while queue:
+            t1, t2 = queue.popleft()
             if not t1 and not t2:
-                return True
+                continue
             if not t1 or not t2:
                 return False
-            return (t1.val == t2.val and 
-                    isMirror(t1.left, t2.right) and
-                    isMirror(t1.right, t2.left))
-        return isMirror(root.left, root.right)
-        
+            if t1.val != t2.val:
+                return False
+            queue.append((t1.left, t2.right))
+            queue.append((t1.right, t2.left))
+        return True
