358ef09d43fd788c86de0e5708c921c8b71d8f22
code25yu <code25yu@gmail.com>
2025-09-18 18:29:12 +0900

    Time: 0 ms (100%), Space: 13.9 MB (37.55%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,10 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        k, now = 0, 1
-        while now < len(nums):
-            if nums[k] != nums[now]:
-                nums[k + 1] = nums[now]
+        
+#         # 1. while
+#         k, now = 0, 1
+#         while now < len(nums):
+#             if nums[k] != nums[now]:
+#                 nums[k + 1] = nums[now]
+#                 k+=1
+#             now+=1
+#         return k + 1   
+        
+        
+        # 2. for
+        k = 0
+        for now in range(1, len(nums)):
+            if nums[now] != nums[k]:
+                nums[k+1] = nums[now]
                 k+=1
             now+=1
         return k + 1
+    
+        
