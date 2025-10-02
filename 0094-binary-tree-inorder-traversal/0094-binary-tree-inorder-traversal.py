e8f818925ac6b0329acceb78add7efb175e03b68
code25yu <code25yu@gmail.com>
2025-10-02 23:16:46 +0900

    Time: 0 ms (100%), Space: 12.5 MB (55.6%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -5,31 +5,31 @@
 #         self.left = left
 #         self.right = right
 
-# # 1. recursive solution
-# class Solution(object):
-#     def inorderTraversal(self, root):
-#         res = []
-#         def dfs(node):
-#             if not node:
-#                 return
-#             dfs(node.left)
-#             res.append(node.val)
-#             dfs(node.right)
-#         dfs(root)
-#         return res
-
-# 2. iterative solution
+# 1. recursive solution
 class Solution(object):
     def inorderTraversal(self, root):
         res = []
-        stack = []
-        now = root
-
-        while now or stack:
-            while now:
-                stack.append(now)
-                now = now.left
-            now = stack.pop()
-            res.append(now.val)
-            now = now.right
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+        dfs(root)
         return res
+    
+# # 2. iterative solution
+# class Solution(object):
+#     def inorderTraversal(self, root):
+#         res = []
+#         stack = []
+#         now = root
+
+#         while now or stack:
+#             while now:
+#                 stack.append(now)
+#                 now = now.left
+#             now = stack.pop()
+#             res.append(now.val)
+#             now = now.right
+#         return res
